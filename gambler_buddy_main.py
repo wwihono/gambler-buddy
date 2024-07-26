2bf628d217f5cfc1e5eae1f3ee9d5b89ce4832dc
tmzfo <tmzfoic@gmail.com>
2024-07-26 22:18:51 +0700

    Main function, initialize vars

--- gambler_buddy_main.py
+++ gambler_buddy_main.py
@@ -1,3 +1,11 @@
 def generate_possibilities():
+
     pass
+
+# Main function
+# Generate player and flop cards and determine percentage that the player has
+# of having the winning hand.
+if __name__ == '__main__':
+    INITIAL_CARDS = list(range(1,53))
+
 
